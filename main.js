c7492d70fd09b9b3bea32c3202b48539eac87c1f
KatMats <kreshetnyak1985@gmail.com>
2022-02-01 18:11:15 +0200

    Lesson1/Homework

---
+++ main.js
@@ -0,0 +1,195 @@
+// let a ='hello'; завдання 1-4 homework
+// console.log (a);
+// alert(a);
+// document.write(a);
+// a='hi';
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b ='owu';
+// console.log(b);
+// alert(b);
+// document.write(b);
+// b='omg';
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c ='com';
+// console.log(c);
+// alert(c);
+// document.write(c);
+// c= 'in';
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
+// d='au';
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let n1 = 1;
+// console.log(n1);
+// alert(n1);
+// document.write(n1);
+// n1= 2;
+// console.log(n1);
+// alert(n1);
+// document.write(n1);
+//
+// let n2 = 10;
+// console.log(n2);
+// alert(n2);
+// document.write(n2);
+// n2= 15;
+// console.log(n2);
+// alert(n2);
+// document.write(n2);
+//
+// let n3 = -999;
+// console.log(n3);
+// alert(n3);
+// document.write(n3);
+// n3= -1000;
+// console.log(n3);
+// alert(n3);
+// document.write(n3);
+//
+// let n123 = 123;
+// console.log(n123);
+// alert(n123);
+// document.write(n123);
+// n123= 456;
+// console.log(n123);
+// alert(n123);
+// document.write(n123);
+//
+// let pi = 3.14;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// pi= 123456;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+//
+// let x = 2.7;
+// console.log(x);
+// alert(x);
+// document.write(x);
+// x=2.8;
+// console.log(x);
+// alert(x);
+// document.write(x);
+//
+//
+// let y = 16;
+// console.log(y);
+// alert(y);
+// document.write(y);
+// y= 15;
+// console.log(y);
+//
+// let t = true;
+// console.log(t);
+// alert(t);
+// document.write(t);
+// t=NaN;
+// console.log(t);
+// alert(t);
+// document.write(t);
+//
+// let f = false;
+// console.log(f);
+// alert(f);
+// document.write(f);
+
+// p.5
+// let firstName = 'Kateryna';
+// let middleName ='Sergeevna';
+// let lastName = 'Matsevetskaya';
+//
+// let person = 'Kateryna'+' '+'Sergeevna'+' '+'Matsevetskaya';
+// console.log(person);
+
+// p.6
+//
+// let name =prompt('Name');
+// let secondName= prompt('Sername');
+// let age=prompt('age');
+
+// p.7
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років"
+// let greet=('Vitayu'+' ');
+// let name=('Kateryna'+' '+'Sergeevna'+' ');
+// let mess=('tobi 36 rokiv');
+// alert (greet + name+ mess);
+
+// p.8,9 За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// let a=100;
+// console.log(typeof a);
+// let b ='100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+// p.10 Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//5 ? 6 -> true
+// let b1=5 < 6;
+// console.log(b1);
+//
+// // 5 ? 6 -> false
+// let b2= 5>6;
+// console.log(b2);
+//
+// // 5 ? 6 -> false
+// let b3= 5==6;
+// console.log(b3);
+//
+// // 5 ? 6 -> false
+// let b4=5===6;
+// console.log(b4);
+//
+// // 10 ? 10 -> true
+// let b5= 10==10;
+// console.log(b5);
+//
+// // 10 ? 10 -> true
+// let b6 = 10===10;
+// console.log(b6);
+//
+// // 10 ? 10 -> false
+// let b7 = 10!==10;
+// console.log(b7);
+//
+// // 10 ? 10 -> false
+// let b8=10!=10;
+// console.log(b8);
+//
+// // 10 ? 10 -> false
+// let b9= 10<10;
+// console.log(b9);
+//
+// // 123 ? '123' -> false
+// let b10 = 123 === '123';
+// console.log(b10);
+//
+// // 123 ? '123' -> true
+// let b11= 123=='123';
+// console.log(b11);
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+//    let a = 5;
+// document.write(str + a + "<br/>");-конкатинація
+
+// document.write(str - a + "<br/>");-15, бо віднімання працює зі стрінгою
+// document.write(str * "2" + "<br/>");-40 множення стрінгового значення
+// document.write(str / 2 + "<br/>");-10.
+
