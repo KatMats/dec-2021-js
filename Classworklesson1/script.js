439aa41ebb7ab0dba93fcc77c9e717662655b6a6
KatMats <kreshetnyak1985@gmail.com>
2022-02-10 15:14:39 +0200

    classwork1

---
+++ Classworklesson1/script.js
@@ -0,0 +1,62 @@
+//1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let num = [25,33,50,255,333,666,7777,10,99,47];
+// console.log(num);
+// let result = num [0]+num [1]+ num [2]+ num [3]+ num [4]+ num [5]+num[6] + num[7] + num [8]+ num [9];
+// console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book = {
+//     name:'Wind',
+//     quantity: 350,
+//     genre :'detective',
+// }
+// console.log( book);
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// let bookOther= {
+//     name: 'Forward',
+//     quantity: 115,
+//     genre: 'nowell',
+// }
+// console.log(bookOther);.
+// 4/Створити маис об'єктів book з наступними полями  - назва,
+// кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let arr =[
+//     {name: 'Sun', quantity: 50, genre: 'fantasy', author: 'Tolkien'},
+//     { name: 'Flower', quantity: 250, genre: 'fantasy', author:'Matveev'},
+//     { name: 'Fly', quantity: 180, genre: 'Adventures', author:'Jooh'},
+//     { name: 'Oho', quantity: 330, genre: 'Adventures', author:'Marshall'},
+// ];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// Напишіть код, який вираховує площу прямокутника висотою 23см.
+// (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+// let a= 23;
+// let b= 10;
+// let s= a*b;
+// console.log(s);
+// 6. Напиши код, який знаходить
+// об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// const PI=3.141592;
+// let heightC= 10;
+// let dC= 4;
+// let r= 1/2*dC;
+// let R= Math.pow(r,2);
+// console.log(R);
+// let v= PI*R*heightC;
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони
+// n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора
+// (потрібно використовувати функцію Math.pow (число, ступінь)
+// або оператор зведення в ступінь **).
+// c**=a** + b**
+// let a= 3;
+// let b= 4;
+// let c2 = Math.pow(a,2)+ Math.pow(b,2);
+// let c= Math.sqrt(c2);
+// console.log(c);
